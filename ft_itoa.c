50370fb9074fad1b2377105bf3cb228bef3487cf
Ismail Raqi <iraqi@e1r4p12.1337.ma>
2021-11-12 05:43:35 +0100

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -33,6 +33,9 @@ int *ft_digitsgeter(int n, size_t size)
     int *res;
     size_t i;
 
+    if (n < 0)
+        n = -n;
+
     res = (int *)malloc(sizeof(int)*size);
     if (res == NULL)
         return NULL;
@@ -51,26 +54,44 @@ char *ft_itoa(int n)
     size_t dc;
     char *res;
     int *digits;
+    int flag;
+    size_t i;
 
-    dc = digitcounter(n);
+    i = -1;
+    if (n < 0)
+        flag = 1;
+    dc = ft_digitcounter(n);
     digits = ft_digitsgeter(n, dc);
+    i = 0;
     if (n < 0)
         dc += 1;
     res = (char *)malloc(sizeof(char)*dc + 1);
-    if (res == NULL)
-        return NULL;
-    
-    
-}
+    if(flag)
+    {
+        res[i] = '-';
+        i++;
+        while (i < dc)
+        {
+            res[i] = digits[i-1] + '0';
+            i++;
+        }
+    }
+    else
+    {
+        while (i < dc)
+        {
+            res[i] = digits[i] + '0';
+            i++;
+        }
+    }
+    res[i] = '\0';
+    return res;
+}   
+
     
 int main(void)
 {
-    int i = 0;
-    int *res = ft_digitsgeter(221155546,9);
-    while (i < 9)
-    {
-        printf("%d\n" ,res[i]);
-        i++;
-    }
+    char *res = ft_itoa(-51512);
+    printf("%s",res);
     return 0;
 }
